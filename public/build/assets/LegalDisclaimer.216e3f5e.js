7f374cc44ff95ae7c5231678192d430ff4a783ef
Sayamuhay <sayamuhay@gmail.com>
2022-11-02 12:56:48 +0700

    initial commit

---
+++ public/build/assets/LegalDisclaimer.216e3f5e.js
@@ -0,0 +1,1 @@
+import{N as r}from"./Navbar.e9caee14.js";import{j as e,F as i,a}from"./app.dd69e3d8.js";import"./index.f6e726b4.js";function d(s){return e(i,{children:[a(r,{}),a("div",{className:"pl-5 pr-5",children:a("div",{className:"bg-white rounded-lg w-auto h-auto mt-5",children:a("div",{children:a("h1",{className:"font-bold",children:"Legal Disclaimer"})})})})]})}export{d as default};
